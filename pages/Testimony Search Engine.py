e7905cfee0de9e59a58b34934565b5a47d416eca
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2022-10-17 19:16:13 +0200

    updated for linking to saha

--- pages/Testimony Search Engine.py
+++ pages/Testimony Search Engine.py
@@ -31,6 +31,14 @@ def load_model():
     bm25 = BM25Okapi(tokenized_corpus)
     return bm25, documents, segments
 
+
+@st.cache(allow_output_mutation=True)
+def load_df():
+    saha_index = pd.read_csv("data/saha_index.csv")
+    return saha_index
+
+saha_index = load_df()
+
 bm25, documents, segments = load_model()
 query = st.sidebar.text_input("Enter Search Here")
 num_results = st.sidebar.number_input("Number of Results", 10,1000)
@@ -39,6 +47,13 @@ if st.sidebar.button("Search"):
     segment_text = bm25.get_top_n(tokenized_query, segments, n=num_results)
     segment_data = bm25.get_top_n(tokenized_query, documents, n=num_results)
     segment_text = [s[0] for s in segment_text]
-    res = {"segment_text": segment_text, "segment_data": segment_data}
+
+    segment_text = [html_file.split("\\")[-1].replace(".json", "") for html_file in segment_text]
+    segment_text = [saha_index.loc[saha_index.id == int(index_num)].src.tolist()[0] for index_num in segment_text]
+    # saha_page = saha_index.loc[saha_index.id == int(index_num)].src.tolist()[0]
+    # st.write(f"<a href='{saha_page}'>Saha Link</a>", unsafe_allow_html=True)
+    segment_text = [f"<a href='{saha_page}'>Saha Link</a>" for saha_page in segment_text]
+    res = {"Testimony": segment_text, "Text": segment_data}
     df = pd.DataFrame(res)
-    st.table(res)
+    res = df.to_markdown()
+    st.markdown(res, unsafe_allow_html=True)
